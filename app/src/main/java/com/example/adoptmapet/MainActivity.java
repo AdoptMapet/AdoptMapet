6fb2e28ec1f7949458041edc80b335251fa03d9b
Gio Aban <gioaban403@gmail.com>
2022-10-30 15:37:28 +0800

    Updated 30/10/2022

--- app/src/main/java/com/example/adoptmapet/MainActivity.java
+++ app/src/main/java/com/example/adoptmapet/MainActivity.java
@@ -5,15 +5,25 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
+import android.view.Display;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -21,8 +31,11 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -31,31 +44,47 @@ public class MainActivity extends AppCompatActivity {
     private NavigationView navigationView;
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle actionBarDrawerToggle;
+
     private RecyclerView postList;
+    private RecyclerAdapter recyclerAdapter;
+    private Context pContext;
+
     private Toolbar mToolbar;
 
     private CircleImageView NavProfileImage;
     private TextView NavProfileUserName;
+    private ImageButton AddNewPostButton;
 
     private FirebaseAuth mAuth;
-    private DatabaseReference UsersRef;
+    private DatabaseReference UsersRef, PostsRef, myRef;
+
+    private ArrayList<Posts> postsArrayList;
 
     String currentUserID;
 
     String text;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        myRef = FirebaseDatabase.getInstance().getReference();
+
         mAuth = FirebaseAuth.getInstance();
         currentUserID = mAuth.getCurrentUser().getUid();
         UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        PostsRef = FirebaseDatabase.getInstance().getReference().child("Posts");
+        postsArrayList = new ArrayList<>();
 
         mToolbar = findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("Home");
+
+        AddNewPostButton = findViewById(R.id.add_new_post_button);
 
 
         drawerLayout = findViewById(R.id.drawable_layout);
@@ -68,9 +97,18 @@ public class MainActivity extends AppCompatActivity {
         //Navigation header
         navigationView = (NavigationView) findViewById(R.id.navigation_view);
 
+        postList = findViewById(R.id.all_users_post_list);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        postList.setLayoutManager(linearLayoutManager);
+        postList.setHasFixedSize(true);
+
+
+
+
         View navView = navigationView.inflateHeaderView(R.layout.navigation_header);
         NavProfileImage = navView.findViewById(R.id.nav_profile_image);
         NavProfileUserName = navView.findViewById(R.id.nav_user_full_name);
+
 
 
         UsersRef.child(currentUserID).addValueEventListener(new ValueEventListener() {
@@ -125,10 +163,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
-
-
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -140,8 +174,80 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+        AddNewPostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SendUserToPostActivity();
+            }
+        });
+
+        //Clear array list
+        ClearAll();
+
+        //Display post
+        DisplayAllUsersPost();
+
     }
 
+    private void DisplayAllUsersPost() {
+
+        Query query = myRef.child("Posts");
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                ClearAll();
+
+                for (DataSnapshot dataSnapshot: snapshot.getChildren() ){
+
+                        //itong mga post value  ay mai babato sa RecyclerAdapter.java
+                    Posts posts = new Posts();
+                         posts.setUid(dataSnapshot.child("uid").getValue().toString());
+                         posts.setDate(dataSnapshot.child("date").getValue().toString());
+                         posts.setTime(dataSnapshot.child("time").getValue().toString());
+                         posts.setDescription(dataSnapshot.child("description").getValue().toString());
+                         posts.setProfileimage(dataSnapshot.child("profileimage").getValue().toString());
+                         posts.setPostimage(dataSnapshot.child("postimage").getValue().toString());
+                         posts.setFullname(dataSnapshot.child("fullname").getValue().toString());
+                         postsArrayList.add(posts);
+}
+
+                recyclerAdapter = new RecyclerAdapter(getApplicationContext(), postsArrayList);
+                postList.setAdapter(recyclerAdapter);
+                recyclerAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+    }
+
+    //Ensure that our list is empty
+    private void ClearAll(){
+        if (postsArrayList != null){
+            postsArrayList.clear();
+
+            if (recyclerAdapter != null){
+                recyclerAdapter.notifyDataSetChanged();
+            }
+        }
+
+        postsArrayList = new ArrayList<>();
+
+    }
+
+
+
+
+    private void SendUserToPostActivity() {
+        Intent addNewPostIntent = new Intent (getApplicationContext(), PostActivity.class);
+        startActivity(addNewPostIntent);
+
+    }
 
 
     //once app run this onStart will run
@@ -222,6 +328,9 @@ public class MainActivity extends AppCompatActivity {
 
 
         switch (item.getItemId()) {
+            case R.id.nav_post:
+                SendUserToPostActivity();
+                break;
             case R.id.nav_profile:
                 Toast.makeText(getApplicationContext(), "Profile", Toast.LENGTH_SHORT).show();
                 break;
@@ -277,3 +386,4 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+
